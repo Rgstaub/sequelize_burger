729687710653c4b46c177abfc2b8e4ba866361f1
Rgstaub <rgstaub@gmail.com>
2017-09-12 17:15:23 -0400

    getting started

--- config/connections.js
+++ config/connections.js
@@ -1,22 +1,44 @@
-const mysql = require('mysql');
+const Sequelize = require('sequelize');
+let env = process.env.NODE || 'development';
+const config = require('./config.json')[env];
 
-// Connection to the mySql database
-let db;
 
-if (process.env.JAWSDB_URL) {
-  db = mysql.createConnection(process.env.JAWSDB_URL)
-} else db = mysql.createConnection({
-    multipleStatements: true,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burger_db"
+
+if (config.use_env_variable) {
+  let sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+sequelize.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
 })
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
 
-db.connect(err => {
-  if (err) throw err;
-  console.log(`Successfull connection to the database`);
-})
+module.exports = sequelize;
 
-// Export the configuration to connect to the database
-module.exports = db;
+// const mysql = require('mysql');
+
+// // Connection to the mySql database
+// let db;
+
+// if (process.env.JAWSDB_URL) {
+//   db = mysql.createConnection(process.env.JAWSDB_URL)
+// } else db = mysql.createConnection({
+//     multipleStatements: true,
+//     host: "localhost",
+//     user: "root",
+//     password: "root",
+//     database: "burger_db"
+// })
+
+// db.connect(err => {
+//   if (err) throw err;
+//   console.log(`Successfull connection to the database`);
+// })
+
+// // Export the configuration to connect to the database
+// module.exports = db;
--- models/burger.js
+++ models/burger.js
@@ -1,30 +1,37 @@
-let orm = require('../config/orm.js');
+const sequelize = require('../config/connections.js');
+
+
+
+
+
+
+
+
 
 // Add the ORM methods
-let burgers = {
-
-  getAll: (cb) => {
-    orm.selectAll(cb);
-  },
-  getSome: (columns, values, cb) => {
-    // Prevent an empty filter form from being submitted
-    if (columns.length === 2 && 
-        values[0] === "No Selection" && 
-        values[1] === "No Selection") return;
-    orm.filteredSelect(columns, values, cb)
-  },
-  addOne: (newBurger) => {
-    orm.insertOne(newBurger);
-  },
-  eatOne: (id) => {
-    orm.updateOne(id);
-  },
-  refreshAll: () => {
-    orm.refreshAll();
-  },
-  clearEaten: () => {
-    orm.clearDevoured();
-  }
-}
+// var burger = {
+//   getAll: (cb) => {
+//     orm.selectAll(cb);
+//   },
+//   getSome: (columns, values, cb) => {
+//     // Prevent an empty filter form from being submitted
+//     if (columns.length === 2 && 
+//         values[0] === "No Selection" && 
+//         values[1] === "No Selection") return;
+//     orm.filteredSelect(columns, values, cb)
+//   },
+//   addOne: (newBurger) => {
+//     orm.insertOne(newBurger);
+//   },
+//   eatOne: (id) => {
+//     orm.updateOne(id);
+//   },
+//   refreshAll: () => {
+//     orm.refreshAll();
+//   },
+//   clearEaten: () => {
+//     orm.clearDevoured();
+//   }
+// }
 
 module.exports = burgers;
